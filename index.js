0cf247505db07eb566351825e02b5cddebf14271
Fasofiso <102464702+Fasofiso@users.noreply.github.com>
2022-04-08 11:48:48 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-function test() {
+const meterToFeet = document.getElementById("MeterToFeet");
+const LitersToGallons = document.getElementById("LitersToGallons");
+const KgToPounds = document.getElementById("KgToPounds");
+
+function convertNumber(event) {
+  const currentValue = event.target.value;
+  meterToFeet.innerText = `${currentValue || 7} meters = ${
+    Math.round(currentValue * 3.2808 * 1000) / 1000
+  } feet | ${currentValue} feet = ${
+    Math.round((currentValue / 3.2808) * 1000) / 1000
+  } meters`;
+  LitersToGallons.innerText = `${currentValue} liters = ${
+    Math.round(currentValue * 0.264172052 * 1000) / 1000
+  } gallons | ${currentValue} gallons = ${
+    Math.round((currentValue / 0.264172052) * 1000) / 1000
+  } liters `;
+  KgToPounds.innerText = `${currentValue} kilos = ${
+    Math.round(currentValue * 2.20462 * 1000) / 1000
+  } pounds | ${currentValue} pounds = ${
+    Math.round((currentValue / 2.20462) * 1000) / 1000
+  } kilos `;
 }
